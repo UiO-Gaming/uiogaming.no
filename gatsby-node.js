48a1e4a844be199b79a6128ea1ad7a670b1bb350
LBlend <lblend@protonmail.com>
2021-07-21 02:16:15 +0200

    add event placeholder pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,9 +3,18 @@ const path = require("path")
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const blogTemplate = path.resolve("./src/components/blogPost.js")
+  const eventTemplate = path.resolve("./src/components/event.js")
   const result = await graphql(`
     query {
       allSanityPost {
+        nodes {
+          _id
+          slug {
+            current
+          }
+        }
+      }
+      allSanityEvent {
         nodes {
           _id
           slug {
@@ -25,6 +34,19 @@ module.exports.createPages = async ({ graphql, actions }) => {
       component: blogTemplate,
       context: {
         id: post._id,
+      },
+    })
+  })
+
+  const events = result.data.allSanityEvent.nodes
+
+  events.forEach(event => {
+    const path = `/event/${event.slug.current}`
+    createPage({
+      path,
+      component: eventTemplate,
+      context: {
+        id: event._id,
       },
     })
   })
---
+++ src/components/event.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const Event = () => {
+  return (
+    <div>
+      <p>Ingenting å se her... enda</p>
+    </div>
+  )
+}
+
+export default Event
