1470763c0a64a0b676cb946a9fcd988e3683ba7e
LBlend <lblend@protonmail.com>
2021-10-01 04:11:04 +0200

    more lowercase e

--- src/components/sections/header.js
+++ src/components/sections/header.js
@@ -29,7 +29,7 @@ const Header = () => {
       <h1>UiO Gaming</h1>
       <p>
         En hobbyforening som ønsker skape et sosialt miljø rundt gaming og
-        E-sport på UiO
+        e-sport på UiO
       </p>
     </header>
   )
