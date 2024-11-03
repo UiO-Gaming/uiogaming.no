e0735a5c97e011d522d298a8330989dbaffd369f
Leander Furumo <leander@furumo.eu>
2024-11-04 00:07:51 +0100

    update 3D picture of office

--- src/components/sections/office.js
+++ src/components/sections/office.js
@@ -21,7 +21,7 @@ const Office = () => {
           id="1"
           sceneId="office"
           config={config}
-          imageSource="/3D_office_2024_04_18.jpg"
+          imageSource="/3D_office_2024_11_03.jpg"
           style={{ width: "100%", aspectRatio: "1 / 1", background: "#000000" }} // yes, apparantly applying a css class to the component doesn't work
         />
       </div>
