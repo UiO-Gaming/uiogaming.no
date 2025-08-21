267bce642299d2854181279a724c17d4f2fa0990
Leander Furumo <leander@furumo.eu>
2025-08-21 03:54:39 +0200

    new 3d office + pc room image

--- src/components/page-sections/office.tsx
+++ src/components/page-sections/office.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useTranslations } from "next-intl"
+import Image from "next/image"
 import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer"
 
 import * as styles from "./office.module.css"
@@ -14,32 +15,29 @@ const Office = () => {
       <div className={styles.container}>
         <p>{t("text")}</p>
         <p>{t("tip")}</p>
-        <div
-          style={{
-            width: "100%",
-            height: "500px",
-            background: "#000000",
-            borderRadius: "8px",
-            overflow: "hidden",
-          }}
-        >
+        <div className={styles.panoramaContainer}>
           <ReactPhotoSphereViewer
-            src="/3D_office_2024_11_03.jpg"
-            height="100%"
-            width="100%"
+            width={"100%"}
+            height={"100%"}
+            src="/3D_office_2025_08_20.jpg"
             defaultZoomLvl={0}
             navbar={true}
           />
         </div>
-        <div style={{ width: "100%" }}>
-          <iframe
-            width="100%"
-            height="500px"
-            src="https://use.mazemap.com/embed.html#v=1&config=uio&zlevel=3&center=10.712463,59.932424&zoom=17&campusid=797&sharepoitype=poi&sharepoi=1000986035&utm_medium=iframe"
-            allow="geolocation"
-            title="Map by MazeMap"
-          ></iframe>
-        </div>
+        <Image
+          src="/pc_room.jpg"
+          alt="Picture of PC-room. Shows 6 gaming PC setups"
+          className={styles.pcRoomImage}
+          width={666}
+          height={500}
+        />
+        <iframe
+          width={"100%"}
+          className={styles.map}
+          src="https://use.mazemap.com/embed.html#v=1&config=uio&zlevel=3&center=10.712463,59.932424&zoom=17&campusid=797&sharepoitype=poi&sharepoi=1000986035&utm_medium=iframe"
+          allow="geolocation"
+          title="Map by MazeMap"
+        ></iframe>
       </div>
     </section>
   )
